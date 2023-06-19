68e6b2ee80564253fdfdfaa3945a11f2a72b4bde
Rushikesh Bagade <rbagade911@gmail.com>
2023-06-19 12:22:03 +0530

    created jopage component

---
+++ my-project/src/components/Jobpage.jsx
@@ -0,0 +1,12 @@
+import React, {useEffect,useState} from 'react'
+import {} from "react-icons"
+import Navbar from './Navbar'
+const Jobpage = () => {
+  return (
+    <>
+        <Navbar/>
+    </>
+  )
+}
+
+export default Jobpage
