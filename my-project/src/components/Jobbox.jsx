eb67a02f75db58775682e99684ec35703c4b3750
Rushikesh Bagade <rbagade911@gmail.com>
2023-06-20 14:17:33 +0530

    updated

--- my-project/src/components/Jobbox.jsx
+++ my-project/src/components/Jobbox.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
-
-const jobbox = ({jobs}) => {
-    console.log(jobs);
+import { Link } from 'react-router-dom'
+// import {turnicateText} from "./Turnicatetext"
+const jobbox = ({ jobs }, title, place) => {
+  const turnicateText = (text) => {
+    return text.substring(0, 40) + "..."
+  }
+  console.log(title + place);
   return (
     <>
+      <div className='p-5 mt-5'>
+        {
+          jobs?.data?.map(i => <div className=' border mt-5 border2 border-black p-5'>
+            <div className="top p-2 ">
+              <p className='text-xl font-bold'>{i.job_title}</p>
+              <p className='text-sm font-medium underline'> By : {i.employer_name}</p>
+              {/* <img className='w-40' src={i.employer_logo} alt="Company logo" srcset="" /> */}
+            </div>
+            <div className="descr p-1">
+              <span className='font-semibold'>Job Description:</span>   {turnicateText(i.job_description)}
+            </div>
+            <Link to={`/job/${title}${place}`} type="button" className="text-white mt-5 bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">View Details</Link>
+          </div>)
+        }
+      </div>
+      {/* //i => employer_name   job_country job_employment_type job_title job_publisher job_city job_apply_link
+  */}
 
     </>
   )
