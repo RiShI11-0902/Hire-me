b85b50a07b2b5a6fc9171dec25ab4a97f0102f9f
Rushikesh Bagade <rbagade911@gmail.com>
2023-06-22 15:01:43 +0530

    created home component

--- my-project/src/components/Home.jsx
+++ my-project/src/components/Home.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import Navbar from './Navbar'
-
+import { mainImg } from '../assets'
 const Home = () => {
   return (
     <>
-        <Navbar/>
+      <Navbar />
+      <div className="main-container bg-[#fbe6e4] w-full h-full min-w-full min-h-screen  flex flex-col-reverse md:flex-row md:justify-around items-center">
+        <div className="title ">
+          <p className=' p-5 mt-10 text-4xl text-[#040101]  leading-snug font-bold main-text'>Find Jobs From Across <br /> The World At One Place.</p>
+          <button className='bg-[#ea7d75] p-5 ml-5 font-semibold rounded-es-2xl'>REGISTER NOW.</button>
+        </div>
+        <div className="imgBox mt-20">
+          <img src={mainImg} alt="" srcset="" />
+        </div>
+      </div>
+      <div className="comapnies min-w-full min-h-screen">
+    COMAPNIES
+      </div>
     </>
   )
 }
