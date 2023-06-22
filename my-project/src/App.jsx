b674a40e4f5eaaff54107e06cd4f6c8ee8510fce
Rushikesh Bagade <rbagade911@gmail.com>
2023-06-22 15:01:24 +0530

    updated

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -7,23 +7,22 @@ import { useAuth0 } from "@auth0/auth0-react";
 import Jobpage from './components/Jobpage';
 import Loginpage from './Loginpage';
 import Jobdeatils from './components/Jobdeatils';
+import Companies from './components/Companies';
 
 function App() {
-  
+
   const { loginWithRedirect, logout, isAuthenticated } = useAuth0();
 
   return (
     <>
-    <Router>
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-        
-          <Route path='/Jobpage' element={ isAuthenticated ? <Jobpage/> : <Loginpage/>}/>
-          <Route path='/job/:id' element={<Jobdeatils/>}/>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/Jobpage' element={isAuthenticated ? <Jobpage /> : <Loginpage />} />
+          <Route path='/job/:id' element={<Jobdeatils />} />
+        </Routes>
+      </Router>
 
-      </Routes>
-    </Router>
-      
     </>
   )
 }
