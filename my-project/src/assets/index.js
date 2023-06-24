3ac4a27ba45c9b007049e545cbcb650fdee45c22
Rushikesh Bagade <rbagade911@gmail.com>
2023-06-24 10:06:35 +0530

    updated

--- my-project/src/assets/index.js
+++ my-project/src/assets/index.js
@@ -1,3 +1,8 @@
 import mainImg from "./main-img.png"
+import img2 from "./img2.png"
+import tanya from "./image-tanya.jpg"
+import next from "./icon-next.svg"
+import prev from "./icon-prev.svg"
+import john from "./image-john.jpg"
 
-export {mainImg}
+export {mainImg,img2,tanya,next,prev,john}
