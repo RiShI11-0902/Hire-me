7ad6d3993a7f2810ba118291e4c58d9f86cd934c
Rushikesh Bagade <rbagade911@gmail.com>
2023-06-26 11:05:16 +0530

    added use Navigate hook

--- my-project/src/components/Jobbox.jsx
+++ my-project/src/components/Jobbox.jsx
@@ -1,15 +1,24 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 // import {turnicateText} from "./Turnicatetext"
-const jobbox = ( { jobs }) => {
+import { BiLinkExternal } from "react-icons/bi";
+import Jobdeatils from './Jobdeatils';
+
+
+const jobbox = ({ jobs }) => {
   const turnicateText = (text) => {
     return text.substring(0, 40) + "..."
   }
-   const id = jobs?.parameters?.query
+
+  const navigate = useNavigate()
+
+  const showDetail = (title,emp,country, city, type, publisher, currency, job_max_salary, link, description) => {
+    navigate("/jobDetails", {state:{title: title,emp_name: emp,country:country,city:city,type:type,publisher:publisher,currency:currency,job_max_salary:job_max_salary,link:link,description:description}}) 
+  }
   return (
     <>
       <div className='p-5 mt-5'>
-        { 
+        {
           jobs?.data?.map(i => <div key={i.job_id} className=' border mt-5 border2 border-black p-5'>
             <div className="top p-2 ">
               <p className='text-xl font-bold'>{i.job_title}</p>
@@ -19,15 +28,14 @@ const jobbox = ( { jobs }) => {
             <div className="descr p-1">
               <span className='font-semibold'>Job Description:</span>   {turnicateText(i.job_description)}
             </div>
-            <Link to={`/job/${id}`} type="button" className="text-white mt-5 bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">View Details</Link>
+            <button onClick={() => showDetail(i.job_title,i.employer_name,i.job_country,i.job_city,i.job_employment_type,i.job_publisher,i.job_salary_currency,i.job_max_salary,i.job_apply_link,i.job_description)} className="text-white mt-5 bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
+              Details
+            </button>
           </div>)
         }
       </div>
-      {/* //i => employer_name   job_country job_employment_type job_title job_publisher job_city job_apply_link
-  */}
-
     </>
   )
-}
-
+}  
 export default jobbox
+
