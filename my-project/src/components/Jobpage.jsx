27817a1b03f1266ca3bf19603610f33563108f72
Rushikesh Bagade <rbagade911@gmail.com>
2023-06-26 11:05:39 +0530

    did some minor changes

--- my-project/src/components/Jobpage.jsx
+++ my-project/src/components/Jobpage.jsx
@@ -7,7 +7,7 @@ import { HiSelector } from "react-icons/hi";
 const Jobpage = () => {
 
   const [data, setData] = useState()
-  const [title, setTitle] = useState("marketing manager")
+  const [title, setTitle] = useState()
   const [place, setPlace] = useState("new york")
   const [date_posted, setDate_posted] = useState("")
   const [remote, setRemote] = useState("")
@@ -15,8 +15,7 @@ const Jobpage = () => {
   const [reuire, setRequire] = useState("")
   const [loading, setLoading] = useState(true)
   //web%20developer%20in%20Texas%2C%20USA
-  const url = `https://jsearch.p.rapidapi.com/search?query=${title} ${place}&page=1&date_posted=all&remote_jobs_only=false&employment_types=INTERN&job_requirements=no_experience`
-
+  const url = `https://jsearch.p.rapidapi.com/search`
   const options = {
     method: 'GET',
     headers: {
@@ -24,31 +23,30 @@ const Jobpage = () => {
       'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
     }
   };
-  console.log(data);
   useEffect(() => {
-    fetch(url, options).then(response => response.json())
+    fetch(`${url}?query=${title}${place}&page=1&date_posted=all&remote_jobs_only=false&employment_types=INTERN&job_requirements=no_experience`, options)
+      .then(response => response.json())
       .then(response => setData(response))
-  }, [])
+  }, [title, place])
+  // console.log(data);
 
-  
-  // const name = `${title} ${place}`
-  // console.log(name);
 
   return (
     <>
       <Navbar />
 
       <div className="container mt-40 md:mt-48  w-full mx-auto">
-        <div className="filters_query">
-          <input type="text" placeholder='Select Title' className='px-4 w-[80%] h-12 my-2 border rounded-lg border-1 border-gray-300 outline-blue-500 ml-10' />
+        <div className="filters_query w-fit">
+          <input type="text" placeholder='Select Title' className='px-4 w-[80%] h-12 my-2 border rounded-lg border-1 border-gray-300 outline-blue-500 ml-10' onKeyUp={(event) => setTitle(event.target.value)} />
           <div className='flex flex-row space-x-8 items-center justify-start'>
-            <input type="text" placeholder='Select Location' className='px-4 h-12 my-2 border rounded-lg border-1 border-gray-300 outline-blue-500 ml-10' />
+            <input type="text" placeholder='Select Location' onKeyUp={(event) => setPlace(event.target.value)} className='px-4 h-12 my-2 border rounded-lg border-1 border-gray-300 outline-blue-500 ml-10' />
             <HiSelector className='text-black' />
             <CiFilter className='text-black' />
           </div>
+          {/* <button className='border border-2 border-yellow-800 w-fit mx-auto ml-20 items-center bg-blue-500' onClick={()=> setQuery()}>Search</button> */}
         </div>
         <div className="list ">
-          <Jobbox  jobs={data} />
+          <Jobbox jobs={data} />
         </div>
       </div>
 
